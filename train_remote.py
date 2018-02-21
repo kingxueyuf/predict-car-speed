a21c659bf73a2cc3ca3f0cf5f1d0704a081c9c05
kingxueyuf <yxue@metamind.io>
2018-02-20 22:20:23 -0800

    add min loss = 3

--- train_remote.py
+++ train_remote.py
@@ -23,7 +23,7 @@ def train():
     util = DatasetUtil()
     criterion = nn.MSELoss()
     lr = 0.001
-    min_loss = 100
+    min_loss = 3
     for epoch in range(10):
         for offset in range(frame_offset_per_time_stamp):  # offset should smaller than frame_offset_per_time_stamp
             running_loss = 0.0
