5a0838a1aaf391ade66af9457c6d3383e234754e
kingxueyuf <yxue@metamind.io>
2018-02-20 21:43:49 -0800

    train strategy

--- train_remote.py
+++ train_remote.py
@@ -24,38 +24,38 @@ def train():
     criterion = nn.MSELoss(False)
     lr = 0.0001
     min_loss = 100
-    for offset in range(frame_offset_per_time_stamp):  # offset should smaller than frame_offset_per_time_stamp
-        running_loss = 0.0
-        epoch = offset
-        for i in range(iteration_per_epoch):
-            x,y = util.fetch_image_and_label(batch_size, time_stamp, frame_offset_per_time_stamp, total_img_num, offset +i*batch_size*time_stamp * frame_offset_per_time_stamp)
-            
-            # wrap them in Variable
-            x = V(th.from_numpy(x).float()).cuda()
-            y = V(th.from_numpy(y).float()).cuda()
-
-            optimizer = optim.Adam(net.parameters(), lr=lr)
-            optimizer.zero_grad()# zero the parameter gradients
-            # forward + backward + optimize
-            predict = net(x)
-
-            print("------------ PREDICT ------------")
-            print(predict)
-            print("------------ LABEL --------------")
-            print(y)
-            loss = criterion(predict, y)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.data[0]
-            if running_loss <= min_loss :
-                min_loss = running_loss
-                print("--- Found smaller loss ---")
-                th.save(net.state_dict(), 'weight/%d_%s.p' % (i, epoch))
-            print('[epoch : %d, iteration : %5d] loss: %.3f' % (epoch, i, running_loss))
+    for epoch in range(100):
+        for offset in range(frame_offset_per_time_stamp):  # offset should smaller than frame_offset_per_time_stamp
             running_loss = 0.0
-        print("Saving model per epoch...")
-        th.save(net.state_dict(), 'weight/epoch_%s.p' % (epoch))
+            for i in range(iteration_per_epoch):
+                x,y = util.fetch_image_and_label(batch_size, time_stamp, frame_offset_per_time_stamp, total_img_num, offset +i*batch_size*time_stamp * frame_offset_per_time_stamp)
+
+                # wrap them in Variable
+                x = V(th.from_numpy(x).float()).cuda()
+                y = V(th.from_numpy(y).float()).cuda()
+
+                optimizer = optim.Adam(net.parameters(), lr=lr)
+                optimizer.zero_grad()# zero the parameter gradients
+                # forward + backward + optimize
+                predict = net(x)
+
+                print("------------ PREDICT ------------")
+                print(predict)
+                print("------------ LABEL --------------")
+                print(y)
+                loss = criterion(predict, y)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.data[0]
+                if running_loss <= min_loss :
+                    min_loss = running_loss
+                    print("--- Found smaller loss ---")
+                    th.save(net.state_dict(), 'weight/epoch%d_offset%d_iter%d.p' % (epoch,offset,i))
+                print('[epoch : %d, offset : %d, i : %d] loss: %.3f' % (epoch, offset, i, running_loss))
+                running_loss = 0.0
+            print("Saving model per offset...")
+            th.save(net.state_dict(), 'weight/epoch%d_offset%d.p' % (epoch,offset))
     print('Finished Training')
 
 train()
