a7dc68fbd642b500e2cce641f57e32e963304351
kingxueyuf <yxue@metamind.io>
2018-02-21 22:50:56 -0800

    modify prediction code

--- predict.py
+++ predict.py
@@ -12,25 +12,25 @@ from datasetutil import DatasetUtil
 
 
 def load_model(): 
-    model_path = "weight/epoch_19.p"
+    model_path = "weight/epoch199.p"
     m = AlexLSTM()
     m.load_state_dict(th.load(model_path))
     m = m.cuda()
     return m
 
-batch_size = 5
-time_stamp = 20
-frame_offset_per_time_stamp = 10
+batch_size = 1
+time_stamp = 30
+image_num_per_time_stamp = 2
+video_length_in_seconds = 17 * 60
 train_dataset = os.listdir("img/")
-total_img_num = len(train_dataset)
-iteration_per_epoch = int(total_img_num / (batch_size*time_stamp))
+iter_per_epoch = int(video_length_in_seconds / (batch_size * time_stamp))
 
 criterion = nn.MSELoss()
 model = load_model()
 util = DatasetUtil()
 
 for i in range(1000):
-    x, y = util.fetch_image_and_label(batch_size, time_stamp, frame_offset_per_time_stamp, total_img_num)
+    x,y = util.fetch_image_and_label(batch_size, time_stamp, image_num_per_time_stamp, video_length_in_seconds - time_stamp)
     x = V(th.from_numpy(x).float()).cuda()
     y = V(th.from_numpy(y).float()).cuda()
     predict = model(x)
